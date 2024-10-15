af98b55f86ed02e087c1e55820e563fcb101d17f
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-10-15 17:46:49 +0900

    Refactor font imports and update metadata in layout.tsx

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -11,9 +11,14 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "custom-bg": "#171A0F",
+      },
+      backgroundImage: {
+        tile: "url('/images/tile.png')",
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
