0e0c64326665726cc4c1167eeda1a119468cd5da
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-10-17 14:23:41 +0900

    Refactor createUser route to separate Firebase user creation and database user creation

--- src/app/api/createUser/route.tsx
+++ src/app/api/createUser/route.tsx
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 import { PrismaClient } from "@prisma/client"
 import { createUserWithEmailAndPassword } from "firebase/auth"
@@ -7,30 +7,60 @@ import { auth } from "@/app/firebaseConfig"
 
 const prisma = new PrismaClient()
 
-export default async function POST(req: NextRequest, res: NextApiResponse) {
-  const body = await req.json()
+interface UserRequestBody {
+  email: string
+  password: string
+  userName: string
+}
 
+export async function POST(req: NextRequest) {
+  const body: UserRequestBody = await req.json()
   try {
-    const userCredential = await createUserWithEmailAndPassword(
-      body.auth,
-      body.email,
-      body.password,
-    )
-    const firebaseUser = userCredential.user
+    // firebaseに認証ユーザーを作成
+    const uid = await firebaseCreateUser(body)
 
     // データベースにユーザー情報を保存
-    await prisma.user.create({
-      data: {
-        firebase_uid: firebaseUser.uid,
-        user_name: body.userName,
-        user_email: body.email,
-        icon_number: 1, // デフォルトのアイコン番号
-      },
-    })
+    saveUserToDatabase(body, uid)
 
-    res.status(200).json({ message: "User registered successfully" })
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 200 },
+    )
   } catch (error: any) {
     console.error("Error registering user:", error)
-    res.status(500).json({ error: error.message || "Error registering user" })
+    return NextResponse.json(
+      { error: error.message || "Error registering user" },
+      { status: 500 },
+    )
   }
+}
+
+/**
+ * firebaseにユーザーを作成し、作成されたuidを返す
+ * @param body : UserRequestBody
+ * @returns User
+ */
+async function firebaseCreateUser(body: UserRequestBody) {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    body.email,
+    body.password,
+  )
+  return userCredential.user.uid
+}
+
+/**
+ * prismaにユーザー情報を保存する
+ * @param body : UserRequestBody
+ * @param userId : string
+ */
+async function saveUserToDatabase(body: UserRequestBody, userId: string) {
+  await prisma.user.create({
+    data: {
+      firebase_uid: userId,
+      user_name: body.userName,
+      user_email: body.email,
+      icon_number: 1, // デフォルトのアイコン番号
+    },
+  })
 }
