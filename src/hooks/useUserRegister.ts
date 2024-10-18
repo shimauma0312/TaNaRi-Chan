1bce621303257c1649df88ad106dcedf49844669
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-10-18 12:57:09 +0900

    Refactor useUserRegister hook to handle user registration and error handling

---
+++ src/hooks/useUserRegister.ts
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
+export const useUserRegister = (e: React.FormEvent) => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [userName, setUserName] = useState("")
+  const [error, setError] = useState("")
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    try {
+      const response = await fetch("/api/createUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, userName }),
+      })
+
+      if (response.ok) {
+        console.log("User registered successfully")
+        router.push("/auth/login") // 成功後の遷移先
+      } else {
+        const errorData = await response.json()
+        setError(errorData.error || "ユーザー登録中にエラーが発生しました。")
+      }
+    } catch (error) {
+      console.error("Error registering user:", error)
+      setError("ユーザー登録中にエラーが発生しました。")
+    }
+  }
+
+  return {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    userName,
+    setUserName,
+    error,
+    handleSubmit,
+  }
+}
