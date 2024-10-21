25aed97a154761b00b40789625e55046062d07ca
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-10-21 15:10:14 +0900

    Refactor page component to add loading functionality

---
+++ src/components/Loader.tsx
@@ -0,0 +1,30 @@
+// components/Loader.tsx
+import React, { useEffect } from "react"
+import loader from "../styles/loader.module.css"
+import fede from "../styles/fede.module.css"
+
+interface LoaderProps {
+  onTimeout: () => void
+  timeout: number
+}
+
+const Loader: React.FC<LoaderProps> = ({ onTimeout, timeout }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onTimeout()
+    }, timeout)
+
+    return () => clearTimeout(timer)
+  }, [onTimeout, timeout])
+
+  return (
+    <section className={(loader.container, fede.fadeinout)}>
+      <div className={loader.loader}> </div>
+      <div className={loader.glitch} data-text="Loading...">
+        Loading...
+      </div>
+    </section>
+  )
+}
+
+export default Loader
