44c04e1f148223106d597c8df3310b88e720ad62
shimauma XL <86759249+shimauma0312@users.noreply.github.com>
2024-12-20 10:59:20 +0000

    feat: Implement POST endpoint for creating articles in the API

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server"
 
 const prisma = new PrismaClient()
+
+// 全ての記事を取得
 export async function GET(): Promise<NextResponse> {
     try {
         const todos = await getArticles()
@@ -13,9 +15,35 @@ export async function GET(): Promise<NextResponse> {
     }
 }
 
+// 記事POST
+export async function POST(req: Request): Promise<NextResponse> {
+    try {
+        const data = await req.json()
+        const newPost = await createArticle(data)
+        return NextResponse.json(newPost)
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: "Failed to create article" }, { status: 500 })
+    }
+}
+
+
 /**
  * 記事リストを取得する
  */
 async function getArticles() {
     return await prisma.post.findMany()
+}
+
+/**
+ * 記事を作成する
+ */
+async function createArticle(data: any) {
+    return await prisma.post.create({
+        data: {
+            title: data.title,
+            content: data.content,
+            author_id: data.author_id,
+        },
+    })
 }
