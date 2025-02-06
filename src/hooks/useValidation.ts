590e10503c129906a73d0412e11a8a7f3731ad06
asa-cording <asahiwork.cording@gmail.com>
2025-02-06 17:28:43 +0900

    fix useValidation

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -2,6 +2,11 @@ import { z } from "zod"
 
 export const useValidation = () => {
   return z.object({
+    username: z
+      .string()
+      .min(6, "パスワードは6文字以上で入力してください。")
+      .max(50, "ユーザー名は50文字以内で入力してください")
+      .regex(/^[ぁ-んァ-ヶ一-龠a-zA-Z0-9]+$/, "記号は使用できません"),
     email: z
       .string()
       .email("正しいメールアドレスを入力してください。")
